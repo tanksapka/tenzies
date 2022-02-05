3095c31f86ef8a20e28157eed32897197b67260d
Tamas Schell <tanksapka@yahoo.com>
2022-02-05 18:51:58 +0100

    Changing logic to keep held dice upon rolling the dice and moving die object generation to separate function. Also adding game instructions.

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,44 @@ import Die from "./components/Die";
 function App() {
   const [dice, setDice] = useState(allNewDice());
 
-  function allNewDice() {
-    return [...Array(10).keys()].map((x) => ({
+  function generateDie() {
+    return {
       value: Math.ceil(Math.random() * 6),
       isHeld: false,
       id: nanoid(),
-    }));
+    };
   }
 
-  function rollDice(event) {
-    setDice(allNewDice());
+  function allNewDice() {
+    return [...Array(10).keys()].map((x) => generateDie());
   }
 
-  const diceElements = dice.map((die) => <Die key={die.id} value={die.value} />);
+  function rollDice() {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld ? die : generateDie();
+      })
+    );
+  }
+
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((oldDie) => {
+        return oldDie.id === id ? { ...oldDie, isHeld: !oldDie.isHeld } : oldDie;
+      })
+    );
+  }
+
+  const diceElements = dice.map((die) => <Die key={die.id} {...die} holdDice={holdDice} />);
 
   return (
     <main className="board-container">
+      <div className="instructions">
+        <h1 className="title">Tenzies</h1>
+        <p className="description">
+          Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+        </p>
+      </div>
       <div className="dice-container">{diceElements}</div>
       <button className="dice-roll" onClick={rollDice}>
         Roll
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,5 +1,9 @@
 import "./Die.css";
 
 export default function Die(props) {
-  return <div className="die-item">{props.value}</div>;
+  return (
+    <div className={`die-item ${props.isHeld && "die-held"}`} onClick={() => props.holdDice(props.id)}>
+      {props.value}
+    </div>
+  );
 }
