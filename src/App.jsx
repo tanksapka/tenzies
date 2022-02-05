9b5b4ecbac8df48182c968949436ca396b5aa8e5
Tamas Schell <tanksapka@yahoo.com>
2022-02-05 18:10:13 +0100

    Changing dice state to store object instead of number, adding nanoid for id/key generation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import "./App.css";
 import { useState } from "react";
+import { nanoid } from "nanoid";
 import Die from "./components/Die";
 
 function App() {
   const [dice, setDice] = useState(allNewDice());
 
   function allNewDice() {
-    return [...Array(10).keys()].map((x) => Math.ceil(Math.random() * 6));
+    return [...Array(10).keys()].map((x) => ({
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    }));
   }
 
   function rollDice(event) {
     setDice(allNewDice());
   }
 
-  const diceElements = dice.map((die, idx) => <Die key={idx} value={die} />);
+  const diceElements = dice.map((die) => <Die key={die.id} value={die.value} />);
 
   return (
     <main className="board-container">
