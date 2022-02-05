2c7a31937ea60bdbce980fa94746a8bdfccded04
Tamas Schell <tanksapka@yahoo.com>
2022-02-05 18:02:25 +0100

    Adding Die component and dynamic generation of 10 dice.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import "./App.css";
+import { useState } from "react";
+import Die from "./components/Die";
 
 function App() {
-  return <div className="App">Tenzies game</div>;
+  const [dice, setDice] = useState(allNewDice());
+
+  function allNewDice() {
+    return [...Array(10).keys()].map((x) => Math.ceil(Math.random() * 6));
+  }
+
+  function rollDice(event) {
+    setDice(allNewDice());
+  }
+
+  const diceElements = dice.map((die, idx) => <Die key={idx} value={die} />);
+
+  return (
+    <main className="board-container">
+      <div className="dice-container">{diceElements}</div>
+      <button className="dice-roll" onClick={rollDice}>
+        Roll
+      </button>
+    </main>
+  );
 }
 
 export default App;
