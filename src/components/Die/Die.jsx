cb80b01ad6f36355eec0f7fd369174cf45e9c294
Tamas Schell <tanksapka@yahoo.com>
2022-02-06 00:14:10 +0100

    Switching numeric display for Die to CSS based dots.

--- src/components/Die/Die.jsx
+++ src/components/Die/Die.jsx
@@ -1,9 +1,14 @@
 import "./Die.css";
 
 export default function Die(props) {
+  const dieFace = [...Array(props.value).keys()].map((idx) => <div key={idx} className="dot"></div>);
+
   return (
-    <div className={`die-item ${props.isHeld && "die-held"}`} onClick={() => props.holdDice(props.id)}>
-      {props.value}
+    <div
+      className={`die-item ${props.isHeld && "die-held"} face-${props.value}`}
+      onClick={() => props.holdDice(props.id)}
+    >
+      {dieFace}
     </div>
   );
 }
